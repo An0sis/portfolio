da4626aaa47c8cdaf1aa8daef56d020d4ec7e4af
Anosis <hantzberg.joric@gmail.com>
2023-05-29 01:34:26 +0700

    Update Project.js

--- website/src/components/Project.js
+++ website/src/components/Project.js
@@ -16,7 +16,7 @@ function Project(){
                 <ProjectCard title="OCR" desc="
 OCR is a 2nd year project at EPITA carried out in groups of 4. The goal is to create a software that allows you to solve a sudoku from an image." image={ocr} tech={projectOcr} link="https://github.com/An0sis/OCR-Epita"/>
                 <ProjectCard title="Licocrypto" desc="This project was made in collaboration with SharkyFlou. Its a program to encrypt and decrypt message with a key or hide message in image using the steganographie. Require : PySimpleGUI, pyperclip, PIL, webbrowser" image={licocrypto} tech={projectLicocrypto} link="https://github.com/An0sis/Projet-LicoCrypto"/>
-                <ProjectCard title="Portfolio" desc="A portfolio website created using React and Tailwind to learn this two technologies" image={website} tech={projectPortfolio} link="https://github.com/An0sis/portfolio"/>
+                <ProjectCard title="Portfolio" desc="A portfolio website created using React and Tailwind to learn these two technologies" image={website} tech={projectPortfolio} link="https://github.com/An0sis/portfolio"/>
                 <ProjectCard title="Eldorado" desc="The legendary world: Eldorado is a MMORPG created during the second semester of the first year at EPITA in a group of four students." image={eldorado} tech={projectEldorado} link="https://github.com/An0sis/Eldorado"/>
             </div>
         </div>
