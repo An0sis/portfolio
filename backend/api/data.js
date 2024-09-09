5b3018fdc264bedc17b1945c132abf1621464803
joric.hantzberg <joric.hantzberg@epita.fr>
2024-09-09 17:07:31 +0200

    data edit

--- backend/api/data.js
+++ backend/api/data.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/data', (req, res) => {
+app.post('/api/data', (req) => {
   const data = req.body;
   console.log('Data received:', data);
-
-  // Append the data to a file
-  fs.appendFile('data.txt', JSON.stringify(data) + '\n', (err) => {
-    if (err) {
-      console.error('Error writing to file', err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.status(200).send('Data received and saved');
-    }
-  });
 });
 
 module.exports = app;
